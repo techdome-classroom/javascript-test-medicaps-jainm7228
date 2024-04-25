e4f8d607de4a0466871d4ca3b7c862a7c0b9e34c
jainm7228 <jainm7228@gmail.com>
2024-04-25 13:32:18 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,21 @@
 function smallestMissingPositiveInteger(nums) {
+  // Create a set to store positive integers
+  const numSet = new Set();
 
-  // Implement the function smallest_missing_positive_integer
+  // Iterate through the array and add positive integers to the set
+  for (const num of nums) {
+      if (num > 0) {
+          numSet.add(num);
+      }
+  }
 
+  // Find the smallest missing positive integer
+  let smallestMissing = 1;
+  while (numSet.has(smallestMissing)) {
+      smallestMissing++;
+  }
+
+  return smallestMissing;
 }
 
 module.exports = smallestMissingPositiveInteger;
