98e431511cb66f2e180cd21609b1c675095d0da5
jainm7228 <jainm7228@gmail.com>
2024-04-25 13:40:38 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,5 +1,15 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+
+    if(s.length === 0)return 0;
+    if(s.length ===1) return 1;
+
+
+    let maxLength = 0;
+    let start = 0;
+    let seen ={};
+
+    
 }
 
 module.exports = { longestSubstring };
